840e66d29dff6f559715aa6773672e15f55b1591
Jun0la <contatojjunola@gmail.com>
2023-03-29 15:56:19 -0300

    Udemy 2#

--- Udemy 1#.cs
+++ Udemy 1#.cs
@@ -5,10 +5,11 @@ namespace TASK
 {
     class Udemy
     {
-
+    
+    /*
     static void Main()
          
-        {
+        {   
             //exercicio #!
             string produto1 = "Computador"; 
             string produto2 = "Mesa de escritorio"; 
@@ -23,10 +24,11 @@ namespace TASK
             +"Computador, cujo preço é $ "+preco1+"\n" 
             +produto2+", cujo preco é $ "+preco2.ToString("F2")+"\n" 
             +"Registro: "+ idade +" anos de idade, codigo "+codigo+" e genero '"+genero+"'\n" 
-            +"Medida com oito casas decimais: "+medida +"\n"+"Arredondado (três casas decimais): "+medida.ToString("F3")+"\n" 
-            +"Separador decimal invariant culture: "+medida.ToString("", CultureInfo.InvariantCulture));
-        
-            /*
+            +"Medida com oito casas decimais: "+medida +"\n"+"Arredondado (três casas decimais): "+medida.ToString("F3", CultureInfo.InvariantCulture)+"\n" 
+            +"Separador decimal invariant culture: "+medida.ToString("F3", CultureInfo.InvariantCulture));
+            
+            
+            
             char letra = '\u10DB';
             object letra = "maqueico";
             int n1 = int.MinValue;
@@ -49,8 +51,8 @@ namespace TASK
             Console.WriteLine("{0} possui {1} anos, e {2:F2} reais", name, idade, saldo);
             Console.WriteLine($"{name} tem {idade} anos, e possui {saldo:F2} reais");
             Console.WriteLine(name+ " tem " + idade+ " anos, e possui "      + " reais");
-            */
+            
         }
-
+    */
     }
 } 
---
+++ Udemy 2#.cs
@@ -0,0 +1,44 @@
+using System;
+
+/*
+namespace TASK
+{
+    class Basics_2
+    {
+        
+    static void Main()
+        
+        {   
+            
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            // double resultado = Maior (n1, n2, n3);
+
+            // System.Console.WriteLine("Maior = " +resultado);
+            
+        }
+
+
+    static int Maior (int a, int b, int c)
+
+        {
+            int M;
+
+            if (a > b && a > c)
+
+                {M = a;}
+
+            else if (b > c)
+
+                {M = b; }
+
+            else { M = c; } 
+
+            return M;  
+        }
+
+    }
+}
+*/ 
