196d487ba8f7a32af7eaed996beb25828eb796f2
Jun0la <contatojjunola@gmail.com>
2023-03-29 21:27:26 -0300

    For, While e If - Basic 2

--- Udemy 2#.cs
+++ Udemy 2#.cs
@@ -1,6 +1,7 @@
 using System;
+using System.Globalization;
 
-/*
+
 namespace TASK
 {
     class Basics_2
@@ -9,7 +10,41 @@ namespace TASK
     static void Main()
         
         {   
+            System.Console.WriteLine("Quantos numeros inteiros: ");
+               
+            int N = int.Parse(Console.ReadLine());
             
+            int soma = 0;
+
+            for (int i = 1; i <= N; i++)
+               
+            {
+                System.Console.Write("Valor  #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+
+            System.Console.WriteLine("Soma = " + soma);
+
+            //---------------------------------------------------------------------//
+
+            System.Console.WriteLine("Digite um numero: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0)
+            
+            {
+
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+            Console.Write("Digite outro numero: ");
+            x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            }
+
+            System.Console.WriteLine("Numero Negativo");
+
+            /*
             int n1 = int.Parse(Console.ReadLine());
             int n2 = int.Parse(Console.ReadLine());
             int n3 = int.Parse(Console.ReadLine());
@@ -17,7 +52,7 @@ namespace TASK
             // double resultado = Maior (n1, n2, n3);
 
             // System.Console.WriteLine("Maior = " +resultado);
-            
+            */
         }
 
 
@@ -41,4 +76,3 @@ namespace TASK
 
     }
 }
-*/ 
