58bcd2b42555822f9c2621228e02999e67a4121c
Jun0la <contatojjunola@gmail.com>
2023-04-08 20:17:27 -0300

    IF Ternário & Switch

--- Udemy#/Udemy 7#.cs
+++ Udemy#/Udemy 7#.cs
@@ -23,5 +23,24 @@ namespace Test
             }
             Console.WriteLine("Dia: " + day);
         }
+
+        // if ternário
+
+        /*
+         double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+            if (preco < 20.0)
+            {
+                desconto = preco * 0.1;
+            }
+            else
+            {
+                desconto = preco * 0.05;
+            }
+
+            Console.WriteLine(desconto);
+        */    
     }
 }
