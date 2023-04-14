189a97547b5e9367d597fb62f40843169ed37fe1
Jun0la <contatojjunola@gmail.com>
2023-04-14 10:52:04 -0300

    Polymorfismo

--- VS Studio/Udemy Exercicio/Course/Course/Program.cs
+++ VS Studio/Udemy Exercicio/Course/Course/Program.cs
@@ -7,18 +7,15 @@ namespace Course
     {
     static void Main(string[] args)
         {
-            Account acc1 = new Account(1001, "Alex", 500.0);
-            Account acc2 = new SavingsAccount(1002, "Annie", 155.0, 0.01);
-            Account acc3 = new BusinessAccount(1003, "Manager", 500.0, 0.01);
+            Account acc1 = new Account(1001, "Annie", 500.00);
+            Account acc2 = new BusinessAccount(1002, "Alexey", 255.00, 0.1);
 
-            acc1.Withdraw(25.0);
-            acc2.Withdraw(15.0);
-            acc3.Deposit(500.00);
-            acc3.Withdraw(155.00);
+            Console.WriteLine(acc1);
+            Console.WriteLine(acc2);
 
-            Console.WriteLine(acc1.Balance);
-            Console.WriteLine(acc2.Balance);
-            Console.WriteLine(acc3.Balance);
+            acc2.Deposit(550.25);
+
+            Console.WriteLine(acc2);
         }
     }
 }
